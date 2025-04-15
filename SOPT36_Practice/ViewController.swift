4e6fec56aac36234760fc79af836f03f7203fb8f
OneTen19 <khy000426@naver.com>
2025-04-16 01:48:51 +0900

    [Feat] 실습 완료

--- SOPT36_Practice/ViewController.swift
+++ SOPT36_Practice/ViewController.swift
@@ -8,33 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let viewWidth = UIScreen.main.bounds.width / 2
-    let viewHeight = UIScreen.main.bounds.height / 4
     
-    private let yellowView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .yellow
-        return view
-    }()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     
-    private let greenView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
-        return view
-    }()
-
-    private let blackView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .black
-        return view
-    }()
-
-    private let blueView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .blue
-        return view
-    }()
-
+    private let redView = UIView()
+    private let orangeView = UIView()
+    private let yellowView = UIView()
+    private let greenView = UIView()
+    private let blueView = UIView()
+    private let purpleView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,38 +28,69 @@ class ViewController: UIViewController {
     }
     
     private func setUI() {
-        self.view.addSubviews(yellowView, greenView, blackView, blueView)
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews(redView, orangeView, yellowView, greenView, blueView, purpleView)
     }
     
     private func setLayout() {
+        redView.backgroundColor = .red
+        orangeView.backgroundColor = .orange
+        yellowView.backgroundColor = .yellow
+        greenView.backgroundColor = .green
+        blueView.backgroundColor = .blue
+        purpleView.backgroundColor = .purple
         
-        NSLayoutConstraint
-            .activate(
-                [
-                    yellowView.topAnchor.constraint(equalTo: view.topAnchor),
-                    yellowView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                    yellowView.widthAnchor.constraint(equalToConstant: viewWidth),
-                    yellowView.heightAnchor.constraint(equalToConstant: viewHeight),
-                    
-                    greenView.topAnchor.constraint(equalTo: yellowView.bottomAnchor),
-                    greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                    greenView.widthAnchor.constraint(equalToConstant: viewWidth),
-                    greenView.heightAnchor.constraint(equalToConstant: viewHeight),
-                    
-                    blackView.topAnchor.constraint(equalTo: greenView.bottomAnchor),
-                    blackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                    blackView.widthAnchor.constraint(equalToConstant: viewWidth),
-                    blackView.heightAnchor.constraint(equalToConstant: viewHeight),
-                    
-                    blueView.topAnchor.constraint(equalTo: blackView.bottomAnchor),
-                    blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                    blueView.widthAnchor.constraint(equalToConstant: viewWidth),
-                    blueView.heightAnchor.constraint(equalToConstant: viewHeight)
-
-                ]
-            )
-
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(greaterThanOrEqualTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor),
+            
+            redView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            redView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            redView.heightAnchor.constraint(equalToConstant: 300),
+            redView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            
+            orangeView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            orangeView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            orangeView.heightAnchor.constraint(equalToConstant: 300),
+            orangeView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            
+            yellowView.topAnchor.constraint(equalTo: redView.bottomAnchor),
+            yellowView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            yellowView.heightAnchor.constraint(equalToConstant: 300),
+            yellowView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            
+            greenView.topAnchor.constraint(equalTo: orangeView.bottomAnchor),
+            greenView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            greenView.heightAnchor.constraint(equalToConstant: 300),
+            greenView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            
+            blueView.topAnchor.constraint(equalTo: yellowView.bottomAnchor),
+            blueView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            blueView.heightAnchor.constraint(equalToConstant: 300),
+            blueView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            blueView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            purpleView.topAnchor.constraint(equalTo: greenView.bottomAnchor),
+            purpleView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            purpleView.heightAnchor.constraint(equalToConstant: 300),
+            purpleView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/2),
+            purpleView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        
     }
-
+    
 }
 
