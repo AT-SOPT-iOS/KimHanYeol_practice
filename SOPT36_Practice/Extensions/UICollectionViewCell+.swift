6d7b85033f7f8b6920bced1f9c66f36f374f342d
OneTen19 <khy000426@naver.com>
2025-04-25 23:51:02 +0900

    [Feat] - 여러 익스텐션들 추가

---
+++ SOPT36_Practice/Extensions/UICollectionViewCell+.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionView+.swift
+//  SOPT36_Practice
+//
+//  Created by OneTen on 4/25/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    // Cell identifier
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+    
+}
