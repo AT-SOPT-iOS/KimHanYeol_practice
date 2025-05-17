2340ab8b8b9595ea6553ab9e0903ead58548fa16
OneTen19 <khy000426@naver.com>
2025-05-17 12:55:09 +0900

    Initial Commit

---
+++ SOPT36_Practice_SwiftUI/SOPT36_Practice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT36_Practice_SwiftUIApp.swift
+//  SOPT36_Practice_SwiftUI
+//
+//  Created by OneTen on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SOPT36_Practice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
