37e1c6381aa2f785c0a7624e722646115e9b443a
OneTen19 <khy000426@naver.com>
2025-05-27 00:03:07 +0900

    Week7 - 7주차 실습

--- SOPT36_Practice_SwiftUI/ContentView.swift
+++ SOPT36_Practice_SwiftUI/ContentView.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let row = [GridItem(.fixed(100)), GridItem(.fixed(100))]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: row, spacing: 16) {
+                ForEach(0..<20) {_ in
+                    VStack {
+                        Rectangle()
+                            .fill(.orange)
+                            .frame(width: 100)
+                    }
+                }
+            }
         }
-        
-        .padding()
     }
 }
 
